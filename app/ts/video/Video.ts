b4f19eed677430747cbaea0e642d51a590f3a372
EKK <xe856423@gmail.com>
2020-08-14 00:10:19 +0800

    新增影片基礎類別

---
+++ app/ts/video/Video.ts
@@ -0,0 +1,9 @@
+class Video {
+    public id: string = '';
+    public title: string = '';
+    public description: string = '';
+    public imageUrl: string = '';
+    public duration: string = '';
+}
+
+export default Video;
