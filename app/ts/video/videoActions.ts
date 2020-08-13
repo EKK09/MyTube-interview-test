b4aab3110e63e5ea3e24f91124790cefb52fcfc4
EKK <xe856423@gmail.com>
2020-08-14 00:21:56 +0800

    新增 fetch 影片列表動作

--- app/ts/video/videoActions.ts
+++ app/ts/video/videoActions.ts
@@ -6,11 +6,33 @@ const PREFIX: string = ActionTypePrefix.VIDEO_LIST;
 
 export const VideoListActionType= {
     SET_VIDEO_LIST: `${PREFIX}_SET_VIDEO_LIST`,
+    FETCH_LIST:`${PREFIX}_FETCH_LIST`,
+    SET_IS_FETCHING: `${PREFIX}_SET_IS_FETCHING`,
+    CANCEL_FETCH_LIST: `${PREFIX}_CANCEL_FETCH_LIST`,
+};
+
+export const fetchVideoListAction = (): AnyAction => {
+    return {
+        type: VideoListActionType.FETCH_LIST,
+    };
 };
 
 export const setVideoListAction = (videos: Video[]): AnyAction => {
     return {
         type: VideoListActionType.SET_VIDEO_LIST,
         videos
+    };
+};
+
+export const setIsFetchingVideoListAction = (isFetching: boolean): AnyAction => {
+    return {
+        type: VideoListActionType.SET_IS_FETCHING,
+        isFetching: isFetching,
+    };
+};
+
+export const cancelFetchVideoListAction = (): AnyAction => {
+    return {
+        type: VideoListActionType.CANCEL_FETCH_LIST,
     };
 };
