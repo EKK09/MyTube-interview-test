4197380a56031072faba15bed2b9de018db77d21
EKK <xe856423@gmail.com>
2020-08-14 00:28:04 +0800

    新增 myTube saga middleware

---
+++ app/ts/myTube/myTubeSaga.ts
@@ -0,0 +1,7 @@
+import { all } from "redux-saga/effects";
+
+function* myTubeSaga() {
+    yield all([]);
+}
+
+export default myTubeSaga;
--- app/ts/myTube/myTubeStore.ts
+++ app/ts/myTube/myTubeStore.ts
@@ -1,8 +1,18 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import myTubeReducer from "./myTubeReducer";
+
+import createSagaMiddleware from "redux-saga";
+import myTubeSaga from "./myTubeSaga";
+
+
+const sagaMiddleware = createSagaMiddleware();
+
 
 const myTubeStore = createStore(
     myTubeReducer,
+    applyMiddleware(sagaMiddleware)
 );
+
+sagaMiddleware.run(myTubeSaga);
 
 export default myTubeStore;
