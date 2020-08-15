74750369e95e76ba857364086bc650d8c41f91f1
EKK <xe856423@gmail.com>
2020-08-16 00:59:03 +0800

    新增讀取中與空資料元件及樣式

---
+++ app/ts/common/components/EmptyList.component.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+const EmptyList = () => (
+    <div className="empty-list-wrapper">
+        <i className="far fa-folder-open"/>
+        <span>Empty here !</span>
+    </div>
+);
+
+export default EmptyList;
---
+++ app/ts/common/components/Loading.component.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+const Loading = () => (
+    <div className="loading-wrapper">
+        <i className="fas fa-sync fa-spin"/>
+        loading...
+    </div>
+);
+
+export default Loading;
