95a659f56dd8aa95ef1c5b7eb560d6854194ad7d
EKK <xe856423@gmail.com>
2020-08-16 21:12:39 +0800

    修改列表標題並隱藏推薦影片的收藏按鈕

--- app/ts/common/constants/ListTitle.ts
+++ app/ts/common/constants/ListTitle.ts
@@ -1,6 +1,5 @@
 export enum ListTitle {
-    VIDEO_LIST_TITLE = "Recommend For You",
-    HOT_VIDEO_LIST_TITLE = "HOT Videos Internationally",
+    VIDEO_LIST_TITLE = "HOT Videos Internationally",
     FAVORITE_LIST_TITLE = "Favorite Videos",
-    POPULAR_VIDEO_TITLE = "Most Popular Internationally"
+    POPULAR_VIDEO_TITLE = "Recommend For You"
 }
