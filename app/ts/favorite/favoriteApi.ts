76211e5ae381e1713470d94d7a56452c2a25b1e4
EKK <xe856423@gmail.com>
2020-08-16 11:18:30 +0800

    調整取得收藏列表 API

--- app/ts/favorite/favoriteApi.ts
+++ app/ts/favorite/favoriteApi.ts
@@ -1,6 +1,33 @@
 import SessionManager from "../common/tool/SessionManager";
+import {AnyAction} from "redux";
+import {QueryParameter} from "../common/constants/ApiConfig";
+import {VideoApiPaths} from "../video/videoApi";
+import HttpMethod from "../common/constants/HttpMethod";
 
-export const fetchFavoriteVideoIdListApi = async (): Promise<string[]> => {
+export const getFetchFavoriteListUrl = (queryParameter: QueryParameter): string => {
+    let url: string = "";
+    url += VideoApiPaths.FETCH_VIDEO_LIST_URL;
+    url += `?part=${queryParameter.part}`;
 
-    return SessionManager.getFavoriteVideoIdList();
+    const favoriteVideoIds: string[] = SessionManager.getFavoriteVideoIdList();
+    url += `&id=${favoriteVideoIds.join(',')}`;
+
+    url += `&key=${queryParameter.key}`;
+    url += `&maxResults=${queryParameter.maxResults}`;
+    url += `&pageToken=${queryParameter.pageToken}`;
+
+    return url;
+};
+
+export const fetchFavoriteListApi = async (queryParameter: QueryParameter): Promise<AnyAction> => {
+
+    return await fetch(
+        getFetchFavoriteListUrl(queryParameter),
+        {
+            method: HttpMethod.GET,
+            headers: {
+                "Content-Type": "application/jsons",
+            },
+        },
+    );
 };
