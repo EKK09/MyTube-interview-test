75a77507c69a6ceb309005ea93fd9b6381974789
EKK <xe856423@gmail.com>
2020-08-16 23:30:48 +0800

    修正最受歡迎影片資訊

--- app/ts/popular/PopularVideo.ts
+++ app/ts/popular/PopularVideo.ts
@@ -2,9 +2,12 @@ import Video from "../video/Video";
 
 const popularVideo: Video = new Video();
 
-popularVideo.title = "Hello! I'm David";
-popularVideo.description = "Hello! I'm David";
-popularVideo.duration = "PT99H11M11S";
+popularVideo.title = "David Wu Sponser Me Video 2020";
+popularVideo.description = `
+	Hello! I'm David. skate for eight years, My favorite trick is heelflips, I can just do it all day
+	actually I haven't land a heelflip over gap, don't worry about that,  I will take it soon.
+`;
+popularVideo.duration = "PT80H11M11S";
 popularVideo.imageUrl = "./image/002.gif";
 popularVideo.favorite = true;
 
